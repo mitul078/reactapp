6be5bd1726578d26dbf80ce876bee6fbb0639d9f
mitul078 <mituljodhani788@gmail.com>
2025-05-31 14:44:53 +0530

    added

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,6 +1,7 @@
 
-
+import './styles/homePage.css'
 const Home = () => {
+  
   return (
     <>
       <div className="homePage">
@@ -29,13 +30,22 @@ const Home = () => {
             </div>
           </div>
         </section>
-        <section className="page-2">
-          <div className="container">
-            <div className="box bg-zinc-200 a">1</div>
-            <div className="box bg-zinc-200 b">2</div>
-            <div className="box bg-zinc-200 c">3</div>
-            <div className="box bg-zinc-200 d">4</div>
-            <div className="box bg-zinc-200 e">5</div>
+        <section className="page-2 " >
+          <div className="icon ">
+            <i className="left ri-arrow-left-s-fill"></i>
+            <i className="right ri-arrow-right-s-fill"></i>
+          </div>
+          <div className="flex-product-show">
+            <div className="product-show-box">1</div>
+            <div className="product-show-box">2</div>
+            <div className="product-show-box">3</div>
+            <div className="product-show-box">4</div>
+            <div className="product-show-box">5</div>
+            <div className="product-show-box">6</div>
+            <div className="product-show-box">7</div>
+            <div className="product-show-box">8</div>
+            <div className="product-show-box">9</div>
+            <div className="product-show-box">10</div>
           </div>
         </section>
       </div>
